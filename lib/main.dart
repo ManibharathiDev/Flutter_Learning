b27c475df572af0be1faaf2d434445597a06dcf1
manibharathi <manibharathir@sugunafoods.com>
2024-01-23 10:26:32 +0530

    Navigation Drawer State Added

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,115 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Suguna Demo Flutter App',
-      home: Home(),
+      home: HomeState(),
     );
   }
 
 }
+
+class HomeState extends StatefulWidget{
+  
+    const HomeState({super.key});
+    @override
+    State<HomeState> createState() {
+      return _MyHomeDrawer();
+    }
+}
+
+class _MyHomeDrawer extends State<HomeState>
+{
+
+  String selectedPage = '';
+
+    @override
+  Widget build(BuildContext context) {
+
+      return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          title: Text('Navigation Drawer',style:TextStyle(
+            color: Colors.white
+          )),
+          backgroundColor: Colors.blue.shade900,
+        ),
+
+        drawer: Drawer(
+          child: ListView(
+            children: [
+
+              // User Accounts Header
+              const UserAccountsDrawerHeader(accountName: Text('Manibharathi'), 
+              accountEmail: Text('manibharathir@sugunafoods.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("M",
+                style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+              ),
+              // Normal Header
+              // const DrawerHeader(
+              //   decoration: BoxDecoration(
+              //     color: Colors.lightBlueAccent
+              //   ),
+              //   child: Text('SFPL')
+
+              //   ),
+                ListTile(
+                  title: const Text('Home'),
+                  leading: Icon(Icons.home),
+                  onTap: (){
+                      //Close the drawer
+                      Navigator.pop(context);
+                       setState(() {
+                         selectedPage = 'HOME';
+                       });
+                  },
+                ),
+
+                ListTile(
+                  title: const Text('About us'),
+                  leading: Icon(Icons.add_moderator_outlined),
+                  onTap: (){
+                      Navigator.pop(context);
+                       setState(() {
+                         selectedPage = 'ABOUT';
+                       });
+                  },
+                )
+            ],
+          ),
+        ),
+        body: Center(
+          child: (selectedPage == 'HOME')?HomeWidget1():AboutWidget1(),
+        ),
+      );
+
+
+    
+  }
+}
+
+class HomeWidget1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('This is Widget1 Home Page')),
+    );
+  }
+
+}
+
+class AboutWidget1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        body: Center(child: Text('This is About Widget1')),
+      );
+  }
+
+}
+
 
 class Home extends StatelessWidget{
   @override
@@ -32,15 +136,28 @@ class Home extends StatelessWidget{
         drawer: Drawer(
           child: ListView(
             children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.lightBlueAccent
-                ),
-                child: Text('SFPL')
 
+              // User Accounts Header
+              const UserAccountsDrawerHeader(accountName: Text('Manibharathi'), 
+              accountEmail: Text('manibharathir@sugunafoods.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("M",
+                style: TextStyle(fontSize: 40.0),
                 ),
+              ),
+              ),
+              // Normal Header
+              // const DrawerHeader(
+              //   decoration: BoxDecoration(
+              //     color: Colors.lightBlueAccent
+              //   ),
+              //   child: Text('SFPL')
+
+              //   ),
                 ListTile(
                   title: const Text('Home'),
+                  leading: Icon(Icons.home),
                   onTap: (){
                       //Close the drawer
                       Navigator.pop(context);
@@ -52,6 +169,7 @@ class Home extends StatelessWidget{
 
                 ListTile(
                   title: const Text('About us'),
+                  leading: Icon(Icons.add_moderator_outlined),
                   onTap: (){
                       Navigator.pop(context);
                        Navigator.push(context, 
